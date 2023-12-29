179c99686253a86da9166632fb782a5a4fec9fa8
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-28 19:04:33 -0500

    Set up associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ApplicationRecord
+    validates :code, :name, presence: true
+
+    has_many :arriving_flights, class_name: "Flight", foreign_key: 'arrival_airport_id'
+    has_many :departings_flights, class_name: "Flight", foreign_key: 'departure_airport_id'
 end
