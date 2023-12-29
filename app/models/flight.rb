77fa88c5b8e86fceee5602717880e9e82d2408c8
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-28 19:04:33 -0500

    Set up associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,6 @@
 class Flight < ApplicationRecord
-  belongs_to :airport
+  validates :departure_airport_id, :arrival_airport_id, :start_datetime, :flight_duration, presence: true
+
+  belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
+  belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
 end
