f263d4f9d0b6dd8304604d265837a45db293182e
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-31 13:12:38 -0500

    Populate flights that matched the user requirements

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,16 @@
 class FlightsController < ApplicationController
     def index
 
-        @departure_airport_options = Flight.pluck(:departure_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.code ] }
+        @departure_airport_options = Flight.pluck(:departure_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.id ] }
 
 
-        @arriving_airport_options =  Flight.pluck(:arrival_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.code ] }
+        @arriving_airport_options =  Flight.pluck(:arrival_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.id ] }
 
         @flight_date_options = Flight.all.map{ |u| [ u.start_datetime, u.id ] }
 
-        @test = params
-
-        @departure_code = params[:departure_code]
+        @flights = Flight.where(
+            departure_airport_id: params[:departure_code],
+            arrival_airport_id: params[:arrival_code]
+          )
     end
 end
