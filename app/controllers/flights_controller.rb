8b00b2d55df39d7e08aa891eb9dcf177665591a4
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-30 19:43:38 -0500

    Fix bugs with params updates
    I added specific route in routes file that allow parameters to be
    updated as expected

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,16 +1,15 @@
 class FlightsController < ApplicationController
     def index
-        @airport_options = Airport.all.map{ |u| [ u.name, u.id ] }
-        @flights = Flight.all
 
-        @arriving_airport_options = Airport.where.not(arriving_flights: nil)
-        .map{ |airport| [airport.name, airport.id] }
-        Flight.select(:arrival_airport_id)
-        @arriving_airport_options =  Flight.pluck(:arrival_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.id ] }
+        @departure_airport_options = Flight.pluck(:departure_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.code ] }
 
 
-        @departure_airport_options = Flight.pluck(:departure_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.id ] }
+        @arriving_airport_options =  Flight.pluck(:arrival_airport_id).uniq.map { |id| Airport.find(id) }.map{ |u| [ u.name, u.code ] }
 
         @flight_date_options = Flight.all.map{ |u| [ u.start_datetime, u.id ] }
+
+        @test = params
+
+        @departure_code = params[:departure_code]
     end
 end
