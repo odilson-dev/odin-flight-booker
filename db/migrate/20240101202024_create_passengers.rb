5decdc83989499c90eb91c5c2dd48cd6c3ede639
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-01 15:21:23 -0500

    Generate Passenger model

---
+++ db/migrate/20240101202024_create_passengers.rb
@@ -0,0 +1,11 @@
+class CreatePassengers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :passengers do |t|
+      t.string :name
+      t.string :email
+      t.references :booking, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
