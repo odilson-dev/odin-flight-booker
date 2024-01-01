5cb98d4e8fd50085af19e5a51890a586526b01d5
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-01 15:25:50 -0500

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_01_201332) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_01_202024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,5 +39,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_201332) do
     t.index ["airport_id"], name: "index_flights_on_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
   add_foreign_key "flights", "airports"
+  add_foreign_key "passengers", "bookings"
 end
