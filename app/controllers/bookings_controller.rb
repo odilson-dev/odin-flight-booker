af507961d6842888d53ff9c6ab6f26e4a36d529b
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-02 09:36:03 -0500

    Set up new action in Bookings_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,9 @@
 class BookingsController < ApplicationController
     def new
+        @flight_id = params[:flight]
+        @flight_object = Flight.find(params[:flight])
+        @number_of_passengers = params[:number_of_passengers]
+        @passenger = Passenger.new
+        @booking = Booking.new
     end
 end
