5985692d82f44575db1776feef7ec60015c68313
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-02 21:46:08 -0500

    Create fields for each passengers in the form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,13 @@ class BookingsController < ApplicationController
         @flight_id = params[:flight]
         @flight_object = Flight.find(params[:flight])
         @number_of_passengers = params[:number_of_passengers]
-        @passenger = Passenger.new
         @booking = Booking.new
+        @number_of_passengers.to_i.times { @booking.passengers.build }
+    end
+
+    private
+    
+    def booking_params
+        params.require(:booking).permit(:flight_number, :departure_date, passengers_attributes: [:name, :email])
     end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
     validates :departure_date, presence: true
 
     has_many :passengers, dependent: :destroy
+    accepts_nested_attributes_for :passengers, allow_destroy: true
 end
